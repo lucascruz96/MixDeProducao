70dbd141cd968bb2383f9d12d9d0672e9938f818
Lucas Cruz <lucascruz96@outlook.com>
2017-11-19 19:24:18 -0200

    Função para carregar a biblioteca do GLPK na Azure

--- api/otimizacao/src/main/java/pucminas/otimizacao/OtimizacaoApplication.java
+++ api/otimizacao/src/main/java/pucminas/otimizacao/OtimizacaoApplication.java
@@ -1,5 +1,7 @@
 package pucminas.otimizacao;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,16 @@ public class OtimizacaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtimizacaoApplication.class, args);
+		loadDll("glpk_4_63.dll");
+		loadDll("glpk_4_63_java.dll");
+	}
+	
+	private static void loadDll(String local) {
+		try {
+			File dll = new File(local);
+			System.load(dll.getAbsolutePath());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
